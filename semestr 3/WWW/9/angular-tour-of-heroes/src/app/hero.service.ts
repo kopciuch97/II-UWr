99dc7152efc25e4a85b470f886f46a433fad0161
SzyKo <kopaszymon97@gmail.com>
2017-12-18 15:35:34 +0100

    lista 9

---
+++ semestr 3/WWW/9/angular-tour-of-heroes/src/app/hero.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {Hero} from './hero';
+import {HEROES} from './mock-heroes';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+import { MessageService } from './message.service';
+
+@Injectable()
+export class HeroService {
+
+  getHeroes(): Hero[] {
+    return HEROES;
+  }
+
+  constructor(private messageService: MessageService) { }
+
+}
