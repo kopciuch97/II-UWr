14309ac38851ab30579290129a012d00273620b1
Szymon <kopaszymon97@gmail.com>
2018-03-05 21:57:52 +0100

    pracownia mp

--- ZPHP/1/1.php
+++ ZPHP/1/1.php
@@ -1,34 +1,41 @@
 <?php
 
 $howmanywords = $_SERVER['argc'];
-for($i = 1; $i< $howmanywords ; $i++){
+
+for ($i = 1; $i < $howmanywords; $i++) {
     $word = $_SERVER['argv'][$i];
 
-    if(strlen($word) % 2 == 1){
-        for($j=1; $j <= strlen($word); $j+=2){
-            $howmanyspaces = (strlen($word)- $j)/2;
-            for($temp=0; $temp <= $howmanyspaces; $temp++){echo " ";};
+    if (strlen($word) % 2 == 1) {
+        for ($j = 1; $j <= strlen($word); $j += 2) {
+            $howmanyspaces = (strlen($word) - $j) / 2;
+            for ($temp = 0; $temp <= $howmanyspaces; $temp++) {
+                echo " ";
+            };
             echo substr($word, $howmanyspaces, $j) . PHP_EOL;
         }
 
-        for ($j= strlen($word)-2; $j>=0 ; $j-= 2){
-            $howmanyspaces = (strlen($word)- $j)/2;
-            for($temp=0; $temp <= $howmanyspaces; $temp++){echo " ";};
+        for ($j = strlen($word) - 2; $j >= 0; $j -= 2) {
+            $howmanyspaces = (strlen($word) - $j) / 2;
+            for ($temp = 0; $temp <= $howmanyspaces; $temp++) {
+                echo " ";
+            }
             echo substr($word, $howmanyspaces, $j) . PHP_EOL;
         }
 
-    }
-
-    else{
-        for($j=0; $j <= strlen($word); $j+=2){
-            $howmanyspaces = (strlen($word)- $j)/2;
-            for($temp=0; $temp <= $howmanyspaces; $temp++){echo " ";};
+    } else {
+        for ($j = 0; $j <= strlen($word); $j += 2) {
+            $howmanyspaces = (strlen($word) - $j) / 2;
+            for ($temp = 0; $temp <= $howmanyspaces; $temp++) {
+                echo " ";
+            };
             echo substr($word, $howmanyspaces, $j) . PHP_EOL;
         }
 
-        for ($j= strlen($word)-2; $j>=0 ; $j-= 2){
-            $howmanyspaces = (strlen($word)- $j)/2;
-            for($temp=0; $temp <= $howmanyspaces; $temp++){echo " ";};
+        for ($j = strlen($word) - 2; $j >= 0; $j -= 2) {
+            $howmanyspaces = (strlen($word) - $j) / 2;
+            for ($temp = 0; $temp <= $howmanyspaces; $temp++) {
+                echo " ";
+            };
             echo substr($word, $howmanyspaces, $j) . PHP_EOL;
         }
 
@@ -36,4 +43,3 @@ for($i = 1; $i< $howmanywords ; $i++){
     }
     echo PHP_EOL;
 }
-?>
