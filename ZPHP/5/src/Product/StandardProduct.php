19571195113754c22242bb10cf7b1853eb879a02
Szymon <kopaszymon97@gmail.com>
2018-04-11 00:09:36 +0200

    Product representation implemented

---
+++ ZPHP/5/src/Product/StandardProduct.php
@@ -0,0 +1,37 @@
+<?php
+namespace src\Product;
+
+use Money\Money;
+
+class StandardProduct implements Product{
+    private $name;
+    private $price;
+    
+    /**
+     * StandardProduct constructor.
+     * @param string $name
+     * @param Money $price
+     */
+    public function __construct(string $name, Money $price)
+    {
+        $this->name = $name;
+        $this->price = $price;
+    }
+    
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+    
+    /**
+     * @return Money
+     */
+    public function getPrice(): Money
+    {
+        return $this->price;
+    }
+    
+}
